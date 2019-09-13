405fbb5f99b8e6cdb890af86cad571a1691a0dab
Gennadiy Gorbulin <gorbulin.gm@gmail.com>
2019-09-13 03:00:22 +0300

    Updates

---
+++ precache-manifest.c5392220f1ad9ec53716c126867e9d22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa2660382ac741978961d834824f21f5",
+    "url": "/react-homework/homework-01/index.html"
+  },
+  {
+    "revision": "f849e36a46a35611a45f",
+    "url": "/react-homework/homework-01/static/css/main.2f8f02ce.chunk.css"
+  },
+  {
+    "revision": "8ddce086fff071375982",
+    "url": "/react-homework/homework-01/static/js/2.1fb8aed9.chunk.js"
+  },
+  {
+    "revision": "f849e36a46a35611a45f",
+    "url": "/react-homework/homework-01/static/js/main.5216b864.chunk.js"
+  },
+  {
+    "revision": "8156181a46229116bea3",
+    "url": "/react-homework/homework-01/static/js/runtime~main.1d418b56.js"
+  }
+]);
